59a0850c6b5c876cec105208e6d3f9610994c61d
kab26 <k.kokov024@mgpz.org>
2025-09-26 09:19:21 +0300

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Banka
+{
+    class BankAccount
+    {
+       private decimal balance;
+       private string name;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Banka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            BankAccount a1 = new BankAccount();
+            Console.WriteLine(a1.Name);
+            Console.WriteLine(a1.Balance);
+            a1.Name = "Stoian";
+            a1.Balance = 120.25m;
+            Console.WriteLine(a1.Name);
+            Console.WriteLine(a1.Balance);
+
+        }
+    }
+}
